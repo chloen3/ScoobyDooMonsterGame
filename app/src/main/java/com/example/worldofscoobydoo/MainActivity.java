8bfa6a3d9cb0b1a4813ffcd451a2d47f3c85bbf4
aditi13020 <aditi13020@gmail.com>
2023-09-23 16:38:58 -0400

    Resolve merge conflicts

--- app/src/main/java/com/example/worldofscoobydoo/MainActivity.java
+++ app/src/main/java/com/example/worldofscoobydoo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.worldofscoobydoo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(v -> {
+            Intent game = new Intent(MainActivity.this, InitialConfiguration.class);
+            startActivity(game);
+            finish();
+        });
+        Button exitButton = findViewById(R.id.exitButton);
+        startButton.setOnClickListener(v -> {
+            finish();
+            System.exit(0);
+        });
     }
 }
