779ac1583b0e1f18bbbee88546ba163aa4a9c7d6
Michael Ferrara <michaelferrara05@gmail.com>
2023-10-03 15:47:58 -0400

    Made Player class singleton and started creating unit tests

---
+++ app/src/test/java/com/example/worldofscoobydoo/Sprint1Tests.java
@@ -0,0 +1,20 @@
+package com.example.worldofscoobydoo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.widget.EditText;
+
+public class Sprint1Tests {
+
+
+    @Test
+    public void nameIsNotNull() {
+//        InitialConfiguration init = new InitialConfiguration();
+//        EditText input = init.findViewById(R.id.editTextText);
+//        String name = input.getText().toString();
+//        assertNotNull(name);
+    }
+
+}
