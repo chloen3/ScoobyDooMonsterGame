f5e1596ff931c732150dccd2c75eb9fa25badbd2
“rsehgal39” <rsehgal39@gatech.edu>
2023-10-10 01:38:24 -0400

    Added some test cases

---
+++ app/src/test/java/com/example/worldofscoobydoo/PlayerTest.java
@@ -0,0 +1,33 @@
+package com.example.worldofscoobydoo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class PlayerTest {
+    @Test
+    public void nameIsBlank() {
+        String name = "";
+        assertFalse(InitialConfiguration.nameIsValid(name));
+    }
+    @Test
+    public void nameIsNull() {
+        String name = null;
+        assertFalse(InitialConfiguration.nameIsValid(name));
+    }
+//    @Test
+//    public void nameIsEmpty() {
+//        String name = " ";
+//        assertFalse(InitialConfiguration.nameIsValid(name));
+//    }
+//    @Test
+//    public void nameIsValid() {
+//        String name = "Ronit";
+//        assertTrue(InitialConfiguration.nameIsValid(name));
+//    }
+}
